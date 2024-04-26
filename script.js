98987a8d8668436829134f34afebfdcfb4c183c6
Andrew Hughes <andyashmore1@gmail.com>
2024-04-26 13:30:20 -0500

    clean up comments and add end notice

--- script.js
+++ script.js
@@ -10,91 +10,14 @@ function getComputerChoice() {
     } else {
       return "scissors";
     }
-}
-
-/* function get
-In this article
-Try it
-Syntax
-Description
-Examples
-Specifications
-Browser compatibility
-See also
-break
-The break statement terminates the current loop or switch statement and transfers program control to the statement following the terminated statement. It can also be used to jump past a labeled statement when used within that labeled statement.
-
-Try it
-
-Syntax
-JS
-Copy to Clipboard
-break;
-break label;
-label Optional
-Identifier associated with the label of the statement to break to. If the break statement is not nested within a loop or switch, then the label identifier is required.
-
-Description
-When break; is encountered, the program breaks out of the innermost switch or looping statement and continues executing the next statement after that.
-
-When break label; is encountered, the program breaks out of the statement labeled with label and continues executing the next statement after that. The break statement needs to be nested within the referenced label. The labeled statement can be any statement (commonly a block statement); it does not have to be another loop statement.
-
-A break statement, with or without a following label, cannot be used at the top level of a script, module, function's body, or static initialization block, even when the function or class is further contained within a loop.
-
-Examples
-break in while loop
-The following function has a break statement that terminates the while loop when i is 3, and then returns the value 3 * x.
-
-JS
-Copy to Clipboard
-function testBreak(x) {
-  let i = 0;
-
-  while (i < 6) {
-    if (i === 3) {
-      break;
-    }
-    i += 1;
-  }
-
-  return i * x;
-}
-break in switch statements
-The following code has a break statement that terminates the switch statement when a case is matched and the corresponding code has run.
-
-JS */ // No idea what this stuff above is, JS just froze for 20 minutes and then it appeared
-
+} //end of getComputerChoice()
 
 function getPlayerChoice() {
   let answer = (prompt("rock paper scissors"));
   return answer.toLowerCase();
-}
+} //end of getPlayerChoice()
 
-//Below is my attempt to create a try catch with functioning error handling but it wasn't working
-// function getPlayerChoice() {
-//   while (true) {
-//     try {
-//       let answer = (prompt("rock paper scissors"));
-//       if (answer != "rock" || answer != "paper" || answer != "scissors") {
-//         new error("You and I both know that's not what I asked for.");
-        
-//       } else {
-//           return answer.toLowerCase();
-//           break;
-//       }
-      
-//     } catch (error) {
-//       throw new error();
-//     }
-//   }
-// } //End of getPlayerChoice()
-
-
-// let computerSelection = function getComputerChoice() {
-  //  Math.floor(Math.random() * 3);
-  //  return computerSelection;
-// }; - This was my original code for the above and didn't work
-
+//user input is compared with computer input to play the round
 function playRound(playerSelection, computerSelection) {
   let result;
   if (playerSelection == computerSelection) {
@@ -119,8 +42,9 @@ function playRound(playerSelection, computerSelection) {
     }
   }
   return result;
-}
+} //end of playRound()
 
+//function to play the game five times
 function playGame() {
   let result;
   for (let i = 0; i < 5; i++) {
@@ -130,11 +54,12 @@ function playGame() {
     console.log(result);
     alert(result);
   }
+  alert("Thank you for playing!")
 }
 
 playGame();
 
-//The game is functioning, but I don't understand why it is asking the first question twice
+
 
 
 
